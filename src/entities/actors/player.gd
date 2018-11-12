68fe070adb966c4662a02501301510fffd82b767
Ross Hadden <rosshadden@gmail.com>
2018-11-12 00:29:18 -0500

    Added basic player movement.

--- src/entities/actors/player.gd
+++ src/entities/actors/player.gd
@@ -1,18 +1,36 @@
-extends Polygon2D
+extends KinematicBody2D
+
+const ZERO = Vector2(0, 0)
+
+const speed = 100.0
+var velocity
 
 func _ready():
 	print("ready")
 	pass
 
 func _input(event):
-	print("input")
+	if Input.is_action_pressed("action.use"):
+		print("use")
 	pass
 
 func _physics_process(dt):
-	pass
+	movement(dt)
+	actions(dt)
 
 func movement(dt):
-	pass
+	velocity = ZERO
+
+	if Input.is_action_pressed("move.up"):
+		velocity += Vector2(0, -1)
+	if Input.is_action_pressed("move.down"):
+		velocity += Vector2(0, 1)
+	if Input.is_action_pressed("move.left"):
+		velocity += Vector2(-1, 0)
+	if Input.is_action_pressed("move.right"):
+		velocity += Vector2(1, 0)
+
+	move_and_slide(velocity * speed)
 
 func actions(dt):
 	pass
